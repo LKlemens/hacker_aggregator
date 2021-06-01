e3b506ee3c93d7b7e274caecd0c030fa57906ba2
LKlemens <klemens.lukaszczyk@gmail.com>
2021-06-01 18:56:02 +0200

    add httpoison to dependencies

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule HackerAggregator.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:httpoison, "~> 1.8"}
     ]
   end
 
