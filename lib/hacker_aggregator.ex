426f1aeaa25f944b5d89efa696ca55af12d644ce
LKlemens <klemens.lukaszczyk@gmail.com>
2021-06-11 11:06:10 +0200

    add get_page/1, get_story/0, get_story/1 to HackerAggregator context

--- lib/hacker_aggregator.ex
+++ lib/hacker_aggregator.ex
@@ -1,5 +1,7 @@
 defmodule HackerAggregator do
   require Logger
+  alias HackerAggregator.Core.Pagination
+  alias HackerAggregator.Core.Story
 
   @moduledoc """
   HackerAggregator keeps the contexts that define your domain
@@ -8,7 +10,29 @@ defmodule HackerAggregator do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  @spec get_stories() :: list()
   def get_stories() do
     HackerAggregator.Boundary.StoryServer.get_stories()
+  end
+
+  @spec get_page(page_number :: integer()) :: %Pagination{}
+  def get_page(page_number) do
+    HackerAggregator.Boundary.StoryServer.get_stories()
+    |> Enum.map(fn {_timestamp, story} -> story end)
+    |> Pagination.new(page_number: page_number)
+  end
+
+  @spec get_story() :: %Story{}
+  def get_story() do
+    HackerAggregator.Boundary.StoryServer.get_stories()
+    |> Enum.random()
+    |> elem(1)
+  end
+
+  @spec get_story(id :: integer()) :: %Story{}
+  def get_story(id) do
+    HackerAggregator.Boundary.StoryServer.get_stories()
+    |> Enum.find(fn {_timestamp, story} -> story.id == id end)
+    |> elem(1)
   end
 end
