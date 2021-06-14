4d69f7648640ef261380a28bf9aed6044b8eab2c
LKlemens <klemens.lukaszczyk@gmail.com>
2021-06-14 06:32:37 +0200

    websocket: send at the beginning of connection already cached stories

--- lib/hacker_aggregator_web/channels/stories_channel.ex
+++ lib/hacker_aggregator_web/channels/stories_channel.ex
@@ -5,23 +5,11 @@ defmodule HackerAggregatorWeb.StoriesChannel do
   require Logger
 
   def join("stories", _message, socket) do
-    send(self(), :after_join)
-    {:ok, socket}
-  end
-
-  def handle_in("all_stories", _message, socket) do
     stories = StoryServer.get_stories()
-    {:reply, {:ok, stories}, socket}
+    {:ok, stories, socket}
   end
 
   def handle_in("new_story", story, socket) do
     {:reply, {:ok, story}, socket}
-  end
-
-  def handle_info(:after_join, socket) do
-    Logger.error("jestem w handle info push")
-    # TODO: find out how to trigger all_stories event from channel
-    push(socket, "all_stories", %{})
-    {:noreply, socket}
   end
 end
